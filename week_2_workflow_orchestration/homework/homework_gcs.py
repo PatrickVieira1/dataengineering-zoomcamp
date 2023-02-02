482b6539e9fc7feff4dcf94ab398cacba3e6aada
Patrickvieira1 <patrickvieirae@gmail.com>
2023-02-02 13:56:42 -0300

    added hw from week 2

--- week_2_workflow_orchestration/homework/homework_gcs.py
+++ week_2_workflow_orchestration/homework/homework_gcs.py
@@ -14,8 +14,8 @@ def fetch(dataset_url: str) -> pd.DataFrame:
 @task(log_prints=True)
 def clean(df = pd.DataFrame) -> pd.DataFrame:
     #Fix dtype issues
-    #df['tpep_pickup_datetime'] = pd.to_datetime(df['tpep_pickup_datetime'])
-    #df['tpep_dropoff_datetime'] = pd.to_datetime(df['tpep_dropoff_datetime'])
+    df['lpep_pickup_datetime'] = pd.to_datetime(df['lpep_pickup_datetime'])
+    df['lpep_dropoff_datetime'] = pd.to_datetime(df['lpep_dropoff_datetime'])
     print(df.head(2))
     print(f'columns: {df.dtypes}')
     print(f'rows: {len(df)}')
@@ -44,8 +44,8 @@ def write_gcs(path: Path) -> None:
 def etl_web_to_gcs() -> None:
     # The main ETL function
     color = "green"
-    year = 2019
-    month = 4
+    year = 2020
+    month = 1
     dataset_file = f"{color}_tripdata_{year}-{month:02}"
     dataset_url = f'https://github.com/DataTalksClub/nyc-tlc-data/releases/download/{color}/{dataset_file}.csv.gz'
     
